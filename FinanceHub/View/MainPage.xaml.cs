4952a7a33c6bb0d412823ec7c7619745eaa65673
Evandro da Silva Amparo <evandro.amparo@gmail.com>
2014-12-14 01:35:33 -0200

    main page

--- FinanceHub/View/MainPage.xaml.cs
+++ FinanceHub/View/MainPage.xaml.cs
@@ -15,16 +15,26 @@ using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
-namespace FinanceHub
+namespace FinanceHub.View
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
-    public sealed partial class MainPage : Page
+  /// <summary>
+  /// An empty page that can be used on its own or navigated to within a Frame.
+  /// </summary>
+  public sealed partial class MainPage : Page
+  {
+    public MainPage()
     {
-        public MainPage()
-        {
-            this.InitializeComponent();
-        }
+      this.InitializeComponent();
     }
+
+    private void AddNewStock(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void RemoveStock(object sender, RoutedEventArgs e)
+    {
+      
+    }
+  }
 }
